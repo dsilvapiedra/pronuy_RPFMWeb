385efa7b3f03f58812b4beab52502ac82a8dd336
Diego SIlva <dsilvapiedra@gmail.com>
2023-02-16 12:07:51 -0300

    Add Prensa Tab. Change drops behaviour

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -12,15 +12,35 @@ for (i = 0; i < acc.length; i++) {
     }
   });}
 */
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
-  }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 
+
+function toggleConfTab(order) {
+  var conftab = document.getElementById("confdrop");
+  if (order) {
+    conftab.style.display = order;
+  }
+  else {
+    conftab.style.display = (conftab.style.display == "block") ? "none" : "block";
+  }
+}
+
+function togglePrensaTab() {
+  var prensatab = document.getElementById("prensadrop");
+  if (order) {
+    prensatab.style.display = order;
+  }
+  else {
+    prensatab.style.display = (prensatab.style.display == "block") ? "none" : "block";
+  }
+}
